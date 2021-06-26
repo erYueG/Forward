0937643aa38a215854f3ad5df2832ce1f7e1495e
lzp1017 <1912099636@qq.com>
2021-06-26 10:55:08 +0800

    first commit

--- goweb/main.go
+++ goweb/main.go
@@ -1,1 +1,10 @@
 package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome come to my page"))
+	})
+	http.ListenAndServe(":8888",nil)
+}
